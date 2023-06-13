2e32b979eacbffaac4dd3e029773b0989a1b1318
Mateus Macedo <105501644+Mateusfdsm1@users.noreply.github.com>
2023-06-13 10:54:41 -0300

    Alteração na Data e Adicionais Java Script

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+      id: 1,
+      author: {
+        avatarUrl: "https://github.com/thiagoamacedo.png",
+        name: 'Thiago Macedo',
+        role: 'Web Developer',
+      },
+      content: [
+       {type: 'paragraph', content: 'Fala galeraa 👋',},
+       {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+        {type: 'link', content: 'jane.design/doctorcare'}
+      ],
+      publishedAt: new Date('2023-06-13 22:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/RaphaelAoki.png',
+      name: 'Rafael Aoki',
+      role: 'Web Developer',
+    },
+    content: [
+     {type: 'paragraph', content: 'Fala galeraa 👋',},
+     {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('2023-06-10 22:00:00'),
+},
+];
+
 
 function App() {
   return (
@@ -15,17 +46,16 @@ function App() {
       <div className= {styles.wrapper}>
           <Sidebar />
         <main>
-          <Post
-          author= "Mateus Macedo"
-          content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nihil quis voluptate exercitationem, corrupti veniam reiciendis vero dolor possimus enim perferendis quia error impedit et necessitatibus iusto culpa blanditiis? Ipsum."
-
-          />
-          <Post
-          author= "Sophia Alves"
-          content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nihil quis voluptate exercitationem, corrupti veniam reiciendis vero dolor possimus enim perferendis quia error impedit et necessitatibus iusto culpa blanditiis? Ipsum." 
-          />
-        </main>
-        
+           {posts.map(post => {
+             return (
+             <Post 
+                author= {post.author}
+                content= {post.content}
+                publishedAt= {post.publishedAt}
+             />
+             )
+           })}
+        </main>   
         </div> 
     </div>
   )
